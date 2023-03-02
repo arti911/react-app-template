007c9fc8c2c78177fbf6d87a73c617c05e9e9f0d
Artur Papikian <34072449+arti911@users.noreply.github.com>
2023-03-02 18:28:03 +0300

    Added new template

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import React from "react";
 
-test('renders learn react link', () => {
+import { App } from "App";
+
+test("renders hello text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hey! I am your new react project!/i);
   expect(linkElement).toBeInTheDocument();
 });
