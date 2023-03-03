905fd3786320937048406229092d51f84543d0bd
Artur Papikian <34072449+arti911@users.noreply.github.com>
2023-03-03 12:38:10 +0300

    Added generate-react-cli

---
+++ templates/component/TemplateName.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./TemplateName.scss";
+
+interface TemplateNameProps {
+  name: string;
+}
+
+export const TemplateName = (props: TemplateNameProps) => {
+  const { name } = props;
+
+  return (
+    <div className={"template-name"}>
+      <h1>{name}</h1>
+    </div>
+  );
+};
