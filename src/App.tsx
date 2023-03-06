77c9fbbf1b83171a6934d54f26c55a31ee835341
Artur Papikian <34072449+arti911@users.noreply.github.com>
2023-03-06 12:49:28 +0300

    added airbnb eslint and prlugin json files

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import "./App.scss";
 
-export const App = () => {
+function App() {
   return (
-    <div className={"app"}>
+    <div className="app">
       <h1>Hey! I am your new react project!</h1>
     </div>
   );
-};
+}
+
+export default App;
